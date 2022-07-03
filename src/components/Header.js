c2e2e4387c436124c7ed2772e5c9fdc86e095204
Karthik <33004854+Karthi1908@users.noreply.github.com>
2022-07-03 23:42:00 +0800

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ import {
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon, SunIcon, MoonIcon } from '@chakra-ui/icons';
 import { useWallet } from '../helper/WalletContext';
-import { CONTRACT_ADDRESS, TOKEN_ADDRESS, wallet,Tezos } from '../helper/tezos';
+import { ContractProvider, CONTRACT_ADDRESS, TOKEN_ADDRESS, wallet,Tezos } from '../helper/tezos';
 import { TezosToolkit, MichelCodecPacker, compose } from '@taquito/taquito';
 import Loading from '../helper/Loading';
 
@@ -219,9 +219,10 @@ export default function Header({ links = [] }) {
                   </MenuItem>
 					<Whitelist />
                   <MenuItem onClick={disconnect}>Disconnect</MenuItem>
-				  
+				  <PortfolioDetails activeAccount={activeAccount}/>
 				  <Deposit />
                   <Redeem />
+				  
                 </MenuList>
               </Menu>
             )}
@@ -250,6 +251,7 @@ export default function Header({ links = [] }) {
                   <MenuItem onClick={() => history('/mypreds')}>
                     My Predictions
                   </MenuItem>
+
                   <MenuItem onClick={disconnect}>Disconnect</MenuItem>
                 </MenuList>
               </Menu>
@@ -262,55 +264,63 @@ export default function Header({ links = [] }) {
 }
 
 
-const PortfolioDetails = () => {
+const PortfolioDetails = (activeAccount) => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const colors = {
     bg: useColorModeValue('purple.100', 'purple.700'),
-    text: useColorModeValue('blue', 'white'),
-	
-	
-  };
-  let portfolio = {};
-	const { connect, disconnect, activeAccount, connected } = useWallet();
+    text: useColorModeValue('blue', 'white'),  };
+  
+	//const { connect, disconnect, activeAccount, connected } = useWallet();
 	const [data, setData] = React.useState(null);
-
+	let portfolio = {};
+	console.log(activeAccount);
+	console.log("conn");
+	
 	React.useEffect( async () => {
-		if (!connected) {
-			await connect();
-		}
-		if (activeAccount) {
-			console.log(activeAccount);
-			console.log(activeAccount.address);
-			const contract = await wallet.at(CONTRACT_ADDRESS);
-			const storage  = await contract.storage();
-			portfolio = await storage.reporters.get(activeAccount.address).then(value => { return ({name : value.name, balance : value.balance.toString(), lockedAmount : value.lockedAmount.toString()})});
-			console.log(portfolio);
-			
-			
 
+    ContractProvider.at(CONTRACT_ADDRESS).then(async (contract) => {
+		//if (!connected) {
+			//await connect();
+			//console.log("connected");
+		//}
+		console.log("connected1");
+		if (activeAccount) {
+			
+			console.log(activeAccount);
+			console.log(activeAccount.activeAccount.address);
+			let address = activeAccount.activeAccount.address;
+			console.log("address ", address);
+			const storage  = await contract.storage();
+			console.log(portfolio);
+			portfolio = await storage.reporters.get(address).then(value => { return ({name : value.name, balance : value.balance.toString(), lockedAmount : value.lockedAmount.toString()})});
+			console.log(portfolio);		
 
 			};
 			
 	console.log(portfolio);
 	setData(portfolio);
 	
+		
+
+    });
 	},[activeAccount]);
-	return (
+
+	return data ?(
 	<>
-      <MenuItem onClick={onOpen}>Portfolio</MenuItem>
+      <MenuItem onClick={onOpen}>Account Details </MenuItem>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Portfolio Details</ModalHeader>
+          <ModalHeader>Account Details</ModalHeader>
           <ModalBody>
 	
 			<Text color={colors.text}> <b> Portfolio </b> </Text>
 			
-        		<Box display="flex" flexDirection="column" flexWrap="wrap">
-				{data.map((pred, i) => {
-					return (
+        		
+				
+					
 						<Box
-							key={i}
+							
 							display="flex"
 							maxWidth="400px"
 							flexDirection="column"
@@ -319,14 +329,14 @@ const PortfolioDetails = () => {
 							padding="5px"
 							margin="5px"
 						>
-								<Text color={colors.text}>User id &nbsp; &nbsp; {pred.name} &nbsp; &nbsp;</Text>
-								<Text color={colors.text}>Balance &nbsp;  &nbsp; {pred.balance} mutez&emsp;</Text>
-								<Text color={colors.text}>LockedAmount &nbsp;  &nbsp; {pred.lockedAmount} mutez&emsp;</Text>
+								<Text color={colors.text}>User id &emsp; &emsp; &emsp; &nbsp;&nbsp;: &nbsp; {data.name} </Text>
+								<Text color={colors.text}>Balance &emsp;  &emsp; &emsp; &nbsp;: &nbsp;{data.balance} mutez</Text>
+								<Text color={colors.text}>Locked Amount   &nbsp; : &nbsp;{data.lockedAmount} mutez</Text>
 								
 						</Box>
-	            );
-                })}
-			</Box>
+	         
+                
+
              
 		
 			</ModalBody>
@@ -334,6 +344,8 @@ const PortfolioDetails = () => {
       </Modal>
     </>
  
+  ):(
+    <Loading />
   );
 };
 
